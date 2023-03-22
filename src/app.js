453fce55065d39825464d49aea9a61d23515a564
Carolina-hora <carolina22hora@gmail.com>
2023-03-22 21:41:34 +0000

    Domain generator into arrays

--- src/app.js
+++ src/app.js
@@ -3,17 +3,18 @@ let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
 const domainLoop = (arrOfPronoun, arrOfAdj, arrOfNoun) => {
-  //let arrOfDomain = [];
+  let arrOfDomain = [];
   for (let i = 0; i < arrOfAdj.length; i++) {
     for (let j = 0; j < arrOfPronoun.length; j++) {
       for (let k = 0; k < arrOfNoun.length; k++) {
-        console.log(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k] + ".com");
+        arrOfDomain.push(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k] + ".com");
       }
     }
   }
+  return arrOfDomain;
 };
+console.log(domainLoop(pronoun, adj, noun));
 
 console.log("Hello World");
-//console.log(domainL
+
 //return arrOfDomain.push(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k])
-domainLoop(pronoun, adj, noun);
