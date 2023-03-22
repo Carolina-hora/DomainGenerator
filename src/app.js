416a87cd4f27a5812a811001417fb70a1d5a21fd
Carolina-hora <carolina22hora@gmail.com>
2023-03-22 22:46:29 +0000

    Extensions added and capitalized letters

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const extensions = [".com", ".net", ".us", ".eu"];
 
-const domainLoop = (arrOfPronoun, arrOfAdj, arrOfNoun) => {
+// const capitalizeFirstLetter = (word) => {
+//   word.charAt(0).toUpperCase() + word.slice(1)
+// }
+
+const domainLoop = (arrOfPronoun, arrOfAdj, arrOfNoun, arrOfExtensions) => {
   //let arrOfDomain = [];
   for (let i = 0; i < arrOfAdj.length; i++) {
     for (let j = 0; j < arrOfPronoun.length; j++) {
       for (let k = 0; k < arrOfNoun.length; k++) {
-        console.log(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k] + ".com");
+        for (let l = 0; l < arrOfExtensions.length; l++) {
+          const capitalizedAdj =
+            arrOfAdj[i].charAt(0).toUpperCase() + arrOfAdj[i].slice(1);
+          const capitalizedNoun =
+            arrOfNoun[k].charAt(0).toUpperCase() + arrOfNoun[k].slice(1);
+          console.log(
+            `${arrOfPronoun[j]}${capitalizedAdj}${capitalizedNoun}${arrOfExtensions[l]}`
+          );
+        }
       }
     }
   }
 };
 
-console.log("Hello World");
-//console.log(domainL
 //return arrOfDomain.push(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k])
-domainLoop(pronoun, adj, noun);
+domainLoop(pronoun, adj, noun, extensions);
