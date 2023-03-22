2fe03bf082c45a6411c94744e43e4a6e3be1461f
Carolina-hora <carolina22hora@gmail.com>
2023-03-22 20:24:02 +0000

    Domain printing in correct order

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const domainLoop = (arrOfPronoun, arrOfAdj, arrOfNoun) => {
+  //let arrOfDomain = [];
+  for (let i = 0; i < arrOfAdj.length; i++) {
+    for (let j = 0; j < arrOfPronoun.length; j++) {
+      for (let k = 0; k < arrOfNoun.length; k++) {
+        console.log(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k] + ".com");
+      }
+    }
+  }
 };
+
+console.log("Hello World");
+//console.log(domainL
+//return arrOfDomain.push(arrOfPronoun[j] + arrOfAdj[i] + arrOfNoun[k])
+domainLoop(pronoun, adj, noun);
